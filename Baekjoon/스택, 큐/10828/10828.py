3848bb0ee02f43511d795baff4b8e5933f848b65
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-04-27 21:52:50 +0900

    [스택] 자료구조, 스택

---
+++ Baekjoon/스택, 큐/10828/10828.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == "push":
+        stack.append(cmd[1])
+    elif cmd[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif cmd[0] == "size":
+        print(len(stack))
+
+    elif cmd[0] == "empty":
+        print(int(len(stack)==0))
+    else:
+        if len(stack) ==0:
+            print(-1)
+        else:
+            print(stack[len(stack)-1])
