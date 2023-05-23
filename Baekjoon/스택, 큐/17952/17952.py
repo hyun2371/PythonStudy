1ccd24415c979abef0f190d8984b0d536f95f5f6
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-05-23 22:07:08 +0900

    [과제는 끝나지 않아] 스택, 구현

---
+++ Baekjoon/스택, 큐/17952/17952.py
@@ -0,0 +1,18 @@
+n = int(input())
+stack = []
+result = 0
+for _ in range(n):
+    task = list(map(int, input().split()))
+    if task[0] == 1:
+        stack.append((task[1],task[2]))
+
+    if len(stack)!=0:
+        score, time = stack.pop()
+        time -= 1
+        if time == 0:
+            result += score
+        else:
+            stack.append((score,time))
+
+
+print(result)
